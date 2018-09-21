43b868cd76fc4c7f94cc1dcaa11b5a43db5ffe31
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2018-09-21 18:13:49 +0100

    commit

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import biens
-from . import locataires
-from . import locations
-from . import etat_des_lieux
-from . import bailleur
-from . import locataire_potentiel
-from . import villes_quartiers
