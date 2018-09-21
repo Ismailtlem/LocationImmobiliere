5e634179b0643ecebc6a18d0b3b8ab860299ed0e
Ismail Tlemçani <34961373+Ismailtlem@users.noreply.github.com>
2018-09-21 18:17:16 +0100

    Add files via upload

---
+++ location_biens/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Location des Biens Immobiliers",
+
+
+    'author': "OdooFreelanceService",
+    'website': "http://www.odoofreelanceservice.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'price': 97,
+    'currency': 'EUR',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mail','portal','resource','analytic'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/biens_view.xml',
+        'views/locataires_view.xml',
+        'views/locataire_potentiel_view.xml',
+        'views/locations_view.xml',
+        'views/etat_des_lieux_view.xml',
+        'views/rapport_paiement.xml',
+        'views/bailleur_view.xml',
+        'views/villes_quartiers_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
