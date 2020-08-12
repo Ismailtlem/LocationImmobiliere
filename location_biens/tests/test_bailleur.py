52a93c7c9af4d3ffdbc0311ea42525ab70c2e616
Ismailtlem <ismail.tlemcani.2016@gmail.com>
2020-08-12 23:23:57 +0200

    add translations and test folder

---
+++ location_biens/tests/test_bailleur.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#from odoo.tests import common
+
+#class TestModelA(common.TransactionCase):
+
+  #  def test_country(self):
+   #         record = self.env['lb.bailleur'].create({'nom': 'Ismail', 'email':'ismail.tlemcani@gmail.com', 'telephone':'0670272931'})
+    #        self.assertEqual(
+     #           record.pays_id.code,
+      #          'MA')
+       #     print('Your test was succesfull!')
